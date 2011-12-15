5671e0582602a309d73cafed247f0d351a14dc84
dad-wd-pakc <pak-loon.cheng@digitalanddirect.com>
2011-12-15 17:58:21 +0000

    Initial functions

--- dom-sixteen.js
+++ dom-sixteen.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 });
 
 var initialiseGame = function () {
-    var canvas = $("#mainCanvas");
+    var canvas = $("#gameCanvas");
     var context = canvas.get(0).getContext("2d");
 
     var canvasHeight = canvas.height();
@@ -18,23 +18,47 @@ var initialiseGame = function () {
     var uiPlay = $("#gamePlayBtn");
     var uiReset = $("#gameResetBtn");
     var uiRemaining = $("#gameAsteroids");
-    var uiScore = $("#gameScore");
+    var uiScore = $(".gameScore");
 
-    var startGame() {
+    var startGame = function () {
+        uiRemaining.html("0");
+        uiScore.html("0");
+        uiStats.show();
+
         playGame = false;
+
+        var platformX;
+        var platformY;
+        var platformOuterRadius;
+        var platformInnerRadius;
 
         animate();
     };
 
-    var init = function() {
-        
+    var init = function () {
+        uiStats.hide();
+        uiComplete.hide();
+
+        uiPlay.click(function (e) {
+            e.preventDefault();
+            uiIntro.hide();
+            startGame();
+        });
+
+        uiReset.click(function (e) {
+            e.preventDefault();
+            uiComplete.hide();
+            startGame();
+        });
     };
 
-    var animate = function() {
+    var animate = function () {
         context.clearRect(0, 0, canvasWidth, canvasHeight);
 
         if (playGame) {
             setTimeout(animate, 33);
         }
     };
+
+    init();
 };
