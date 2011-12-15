15a379cddbe8c9aebfd4a296fc7f9fe83c52759e
dad-wd-pakc <pak-loon.cheng@digitalanddirect.com>
2011-12-15 17:35:00 +0000

    Continuing the build of the game

--- dom-sixteen.js
+++ dom-sixteen.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
-    initGame();
+    initialiseGame();
 });
 
-var animationLoop = function () {
+var initialiseGame = function () {
     var canvas = $("#mainCanvas");
     var context = canvas.get(0).getContext("2d");
 
-    initGame();
+    var canvasHeight = canvas.height();
+    var canvasWidth = canvas.width();
+
+    var playGame;
+
+    var ui = $("#gameUI");
+    var uiIntro = $("#gameIntro");
+    var uiStats = $("#gameStats");
+    var uiComplete = $("#gameComplete");
+    var uiPlay = $("#gamePlayBtn");
+    var uiReset = $("#gameResetBtn");
+    var uiRemaining = $("#gameAsteroids");
+    var uiScore = $("#gameScore");
+
+    var startGame() {
+        playGame = false;
+
+        animate();
+    };
+
+    var init = function() {
+        
+    };
+
+    var animate = function() {
+        context.clearRect(0, 0, canvasWidth, canvasHeight);
+
+        if (playGame) {
+            setTimeout(animate, 33);
+        }
+    };
 };
